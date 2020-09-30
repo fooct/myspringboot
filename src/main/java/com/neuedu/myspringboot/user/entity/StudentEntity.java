bf9b9ec78925ca3e4cca68656b69e51004656e0f
root <814569256@qq.com>
2020-09-30 08:34:20 +0800

    1

--- src/main/java/com/neuedu/myspringboot/user/entity/StudentEntity.java
+++ src/main/java/com/neuedu/myspringboot/user/entity/StudentEntity.java
@@ -1,11 +1,17 @@
 package com.neuedu.myspringboot.user.entity;
 
+import lombok.Data;
+
+import javax.persistence.Id;
+import javax.persistence.Table;
 import java.io.Serializable;
 
+@Data
+@Table(name = "student")
 public class StudentEntity implements Serializable {
 
     public static final long serialVersionUID = 1L;
-
+    @Id
     private Integer id;
     private String useName;
     private Integer chinese;
